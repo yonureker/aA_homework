c866606620d6a0f5a7cf20ff1cd92cfb6521eca2
yonureker <yonureker@gmail.com>
2019-06-05 08:14:37 -0700

    Add W4D3 homework

---
+++ W4D3/my_cool_app/app/models/user.rb
@@ -0,0 +1,41 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  username        :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ApplicationRecord
+  validates :username, :session_token, presence: true
+  validates :password_digest, presence: { message: 'Password can\'t be blank' }
+  validates :password, length: { minimum: 6, allow_nil: true }
+  before_validation :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
+
+  def reset_session_token
+    self.session_token = User.generate_session_token
+    self.save!
+    self.session
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+end
